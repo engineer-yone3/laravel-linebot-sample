af37b9316591ed6dd54a1cf36a3b24a0956500d6
yone3 <engineer.yone3@gmail.com>
2022-11-25 00:10:54 +0900

    画像処理追加

--- projdir/app/Services/Event/LineImageEvent.php
+++ projdir/app/Services/Event/LineImageEvent.php
@@ -3,6 +3,8 @@
 namespace App\Services\Event;
 
 use App\Services\Bot\LineApiServiceInterface;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use LINE\LINEBot;
 use LINE\LINEBot\Event\MessageEvent\ImageMessage;
 use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
@@ -29,6 +31,19 @@ class LineImageEvent {
             $textMessage = new TextMessageBuilder('送られた画像はこちらです');
             $messages = new MultiMessageBuilder([$textMessage, $imageMessage]);
             $this->service->replyMessage($bot, $event->getReplyToken(), $messages);
+        } else {
+            $messageId = $event->getMessageId();
+            $response = $bot->getMessageContent($messageId);
+            if ($response->isSucceeded()) {
+                $fileName = Str::random(255);
+                Storage::disk('public')->put($fileName, $response->getRawBody());
+                $url = Storage::disk('public')->url($fileName);
+
+                $imageMessage = new ImageMessageBuilder($url,$url);
+                $textMessage = new TextMessageBuilder('送られた画像はこちらです');
+                $messages = new MultiMessageBuilder([$textMessage, $imageMessage]);
+                $this->service->replyMessage($bot, $event->getReplyToken(), $messages);
+            }
         }
     }
 }
