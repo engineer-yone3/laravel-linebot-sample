b0ee5e11a9aec95b137525b06ecbcf911b573f8f
yone3 <engineer.yone3@gmail.com>
2022-11-26 13:20:04 +0900

    画像処理を修正

--- projdir/app/Services/Event/LineImageEvent.php
+++ projdir/app/Services/Event/LineImageEvent.php
@@ -10,6 +10,7 @@ use LINE\LINEBot\Event\MessageEvent\ImageMessage;
 use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use Throwable;
 
 class LineImageEvent {
     public function __construct(
@@ -21,37 +22,43 @@ class LineImageEvent {
 
     public function execute(ImageMessage $event, LINEBot $bot): void
     {
-        $contentProvider = $event->getContentProvider();
+        try {
+            $contentProvider = $event->getContentProvider();
 
-        logger()->debug('[event data]');
-        logger()->debug(print_r($event, true));
-        if ($contentProvider->isExternal()) {
-            logger()->debug('外部ファイル');
-            $imageMessage = new ImageMessageBuilder(
-                $contentProvider->getOriginalContentUrl(),
-                $contentProvider->getPreviewImageUrl()
-            );
-            $textMessage = new TextMessageBuilder('送られた画像はこちらです');
-            $builder = new MultiMessageBuilder();
-            $builder->add($textMessage);
-            $builder->add($imageMessage);
-            $this->service->replyMessage($bot, $event->getReplyToken(), $builder);
-        } elseif ($contentProvider->isLine()) {
-            logger()->debug('Lineファイル');
-            $messageId = $event->getMessageId();
-            $response = $bot->getMessageContent($messageId);
-            if ($response->isSucceeded()) {
-                $fileName = Str::random(255);
-                Storage::disk('public')->put($fileName, $response->getRawBody());
-                $url = Storage::disk('public')->url($fileName);
-
-                $imageMessage = new ImageMessageBuilder($url,$url);
+            logger()->debug('[event data]');
+            logger()->debug(print_r($event, true));
+            if ($contentProvider->isExternal()) {
+                logger()->debug('外部ファイル');
+                $imageMessage = new ImageMessageBuilder(
+                    $contentProvider->getOriginalContentUrl(),
+                    $contentProvider->getPreviewImageUrl()
+                );
                 $textMessage = new TextMessageBuilder('送られた画像はこちらです');
                 $builder = new MultiMessageBuilder();
                 $builder->add($textMessage);
                 $builder->add($imageMessage);
                 $this->service->replyMessage($bot, $event->getReplyToken(), $builder);
+            } elseif ($contentProvider->isLine()) {
+                logger()->debug('Lineファイル');
+                $messageId = $event->getMessageId();
+                $response = $bot->getMessageContent($messageId);
+                if ($response->isSucceeded()) {
+                    logger()->debug('content取得成功');
+                    $fileName = Str::random(255);
+                    Storage::disk('public')->put($fileName, $response->getRawBody());
+                    $url = Storage::disk('public')->url($fileName);
+    
+                    $imageMessage = new ImageMessageBuilder($url,$url);
+                    $textMessage = new TextMessageBuilder('送られた画像はこちらです');
+                    $builder = new MultiMessageBuilder();
+                    $builder->add($textMessage);
+                    $builder->add($imageMessage);
+                    $this->service->replyMessage($bot, $event->getReplyToken(), $builder);
+                }
             }
+        } catch (Throwable $th) {
+            logger()->error($th->getMessage());
+            logger()->error($th->getTraceAsString());
         }
     }
 }
